276feef297d5652378afcd4eb3bb302f17204de4
Jón Levy <nonni@nonni.cc>
2015-09-26 18:49:06 +0000

    ..

--- config/env/production.js
+++ config/env/production.js
@@ -54,18 +54,7 @@ module.exports = {
   // ssl: false,
   // stringify: true
 
-  },
-
-
-  /***************************************************************************
-   * Set the default database connection for models in the production        *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
-   
-   models: {
-    connection: 'someMongodbServer'     
-   },
-   
+  },   
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
